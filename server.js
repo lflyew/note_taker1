fd54b59aecfa2f4124de31bc9261b4b8b9db709d
Latoya Lyew <nursebae@Latoyas-MBP.hsd1.ga.comcast.net>
2022-11-20 20:20:16 -0500

    please workl

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const apiroutes = require("./routes/index.js");
-const apiroutes = require("./routes/index");
+const apiroutes = require("./routes/routesapi");
 const htmlroutes = require("./routes/routeshtml.js");
 
 const PORT = process.env.PORT || 3001;
